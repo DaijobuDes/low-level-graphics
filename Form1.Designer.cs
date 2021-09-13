bad891b4bfc8737e602b210f27b04a5068ba199e
Kate Aubrey Cellan <cellankateaubrey@gmail.com>
2021-09-13 12:13:56 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace csharp_low_level_graphics
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(905, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.SplitButton;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 441);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please click the button to enable/disable animations.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 459);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start/Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(553, 458);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 527);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharp_low_level_graphics
+{
+    public partial class Form1 : Form
+    {
+        Graphics g1;
+        private int x = 10, y = 700;
+        private bool run = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Box size 929, 527
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            g1 = e.Graphics;
+
+            // House
+            g1.DrawRectangle(Pens.White, new Rectangle(350, 225, 300, 200));
+            g1.DrawRectangle(Pens.White, new Rectangle(475, 350, 50, 75));
+            g1.DrawEllipse(Pens.White, new Rectangle(480, 375, 10, 10));
+            // Window
+            g1.DrawRectangle(Pens.White, new Rectangle(400, 250, 50, 50));
+            g1.DrawRectangle(Pens.White, new Rectangle(550, 250, 50, 50));
+            // Roof
+            g1.DrawLine(Pens.White, new Point(350, 225), new Point(500, 0));
+            g1.DrawLine(Pens.White, new Point(650, 225), new Point(500, 0));
+
+            // Tree 1
+            // Trunk
+            g1.DrawArc(Pens.RosyBrown, new Rectangle(100, 300, 45, 100), -90, 180);
+            g1.DrawArc(Pens.RosyBrown, new Rectangle(175, 300, 45, 100), 270, -180);
+
+            // Leaves
+            // Left
+            g1.DrawArc(Pens.LimeGreen, new Rectangle(100, 250, 45, 45), 83, 180);
+            g1.DrawArc(Pens.LimeGreen, new Rectangle(110, 225, 45, 45), 125, 180);
+            // Center
+            g1.DrawArc(Pens.LimeGreen, new Rectangle(135, 210, 45, 45), 180, 180);
+            // Right
+            g1.DrawArc(Pens.LimeGreen, new Rectangle(163, 225, 45, 45), 225, 180);
+            g1.DrawArc(Pens.LimeGreen, new Rectangle(175, 252, 45, 45), 260, 180);
+
+            // Tree 2
+            // Trunk
+            g1.DrawArc(Pens.RosyBrown, new Rectangle(700, 300, 45, 100), -90, 180);
+            g1.DrawArc(Pens.RosyBrown, new Rectangle(775, 300, 45, 100), 270, -180);
+
+            // Leaves
+            // Left
+            g1.DrawArc(Pens.LimeGreen, new Rectangle(700, 250, 45, 45), 83, 180);
+            g1.DrawArc(Pens.LimeGreen, new Rectangle(710, 225, 45, 45), 125, 180);
+            // Center
+            g1.DrawArc(Pens.LimeGreen, new Rectangle(735, 210, 45, 45), 180, 180);
+            // Right
+            g1.DrawArc(Pens.LimeGreen, new Rectangle(763, 225, 45, 45), 225, 180);
+            g1.DrawArc(Pens.LimeGreen, new Rectangle(775, 252, 45, 45), 260, 180);
+
+            // Sun
+            g1.DrawArc(Pens.Yellow, new Rectangle(840, -10, 100, 100), 0, 360);
+
+            // Cloud
+            g1.DrawArc(Pens.SkyBlue, new Rectangle(x, 100, 45, 45), 90, 180);
+            g1.DrawArc(Pens.SkyBlue, new Rectangle(x + 20, 90, 45, 45), 155, 180);
+            g1.DrawArc(Pens.SkyBlue, new Rectangle(x + 55, 90, 45, 45), 165, 180);
+            g1.DrawArc(Pens.SkyBlue, new Rectangle(x + 85, 90, 45, 45), 190, 180);
+            g1.DrawArc(Pens.SkyBlue, new Rectangle(x + 115, 105, 45, 45), 225, 180);
+            g1.DrawArc(Pens.SkyBlue, new Rectangle(x + 115, 115, 45, 45), 300, 180);
+            g1.DrawArc(Pens.SkyBlue, new Rectangle(x + 90, 125, 45, 45), 360, 180);
+            g1.DrawArc(Pens.SkyBlue, new Rectangle(x + 55, 125, 45, 45), 360, 180);
+            g1.DrawArc(Pens.SkyBlue, new Rectangle(x + 20, 120, 45, 45), 30, 180);
+
+            // Cloud 2
+            g1.DrawArc(Pens.SkyBlue, new Rectangle(y, 80, 45, 45), 90, 180);
+            g1.DrawArc(Pens.SkyBlue, new Rectangle(y + 20, 70, 45, 45), 155, 180);
+            g1.DrawArc(Pens.SkyBlue, new Rectangle(y + 55, 70, 45, 45), 165, 180);
+            g1.DrawArc(Pens.SkyBlue, new Rectangle(y + 85, 70, 45, 45), 190, 180);
+            g1.DrawArc(Pens.SkyBlue, new Rectangle(y + 115, 85, 45, 45), 225, 180);
+            g1.DrawArc(Pens.SkyBlue, new Rectangle(y + 115, 95, 45, 45), 300, 180);
+            g1.DrawArc(Pens.SkyBlue, new Rectangle(y + 90, 105, 45, 45), 360, 180);
+            g1.DrawArc(Pens.SkyBlue, new Rectangle(y + 55, 105, 45, 45), 360, 180);
+            g1.DrawArc(Pens.SkyBlue, new Rectangle(y + 20, 100, 45, 45), 30, 180);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (run)
+            {
+                timer1.Enabled = true;
+                run = false;
+                return;
+            }
+            timer1.Enabled = false;
+            run = true;
+        }
+
+        // Exit
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            x += 5;
+            y -= 5;
+
+            if (x >= 1000)
+            {
+                x = -100;
+            }
+
+            if (y <= -100)
+            {
+                y = 1000;
+            }
+
+            this.Refresh();
+        }
+    }
+}
